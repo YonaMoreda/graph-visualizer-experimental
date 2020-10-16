805a2ac7b73412cf539590568a48e7dc52b19315
YonaMoreda <yokots3q@gmail.com>
2020-10-16 19:50:09 +0200

    added matterjs engine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import Matter from "matter-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        // module aliases
+        let Engine = Matter.Engine,
+            Render = Matter.Render,
+            World = Matter.World,
+            Bodies = Matter.Bodies,
+            Mouse = Matter.Mouse,
+            MouseConstraint = Matter.MouseConstraint;
+
+        // create an engine
+        let engine = Engine.create();
+        engine.world.gravity.y = 0; // turn off gravity
+
+        // create a renderer
+        const render = Render.create({
+            element: document.body,
+            engine: engine,
+            options: {
+                height: window.innerHeight - 4,
+                width: window.innerWidth,
+                wireframes: false
+            }
+        });
+
+        // create two boxes and a ground
+        const boxA = Bodies.rectangle(400, 200, 80, 80);
+        const boxB = Bodies.rectangle(450, 50, 80, 80);
+        const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+
+        // add all of the bodies to the world
+        World.add(engine.world, [boxA, boxB, ground]);
+
+        // adding mouse input (constraint)
+        const mouse = Mouse.create(render.canvas),
+            mouseConstraint = MouseConstraint.create(engine, {
+                mouse: mouse,
+                constraint: {
+                    stiffness: 0.2,
+                    render: {
+                        visible: false
+                    }
+                }
+            });
+        World.add(engine.world, mouseConstraint);
+
+        //creating spring constraint
+        const constraint = Matter.Constraint.create({
+            bodyA: boxA,
+            bodyB: boxB,
+            restitution: 0.97,
+            length: 150,
+            stiffness: 0.01
+        });
+        World.add(engine.world, constraint);
+
+        var GraphNode = require("./GraphNode");
+
+        World.add(engine.world, GraphNode);
+
+        Engine.run(engine);
+        Render.run(render);
+    }
+
+    render() {
+        return <div className="App"/>;
+    }
 }
 
 export default App;
