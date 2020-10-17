77d21f85224803a383e40d855696a44989d7f17b
YonaMoreda <yokots3q@gmail.com>
2020-10-17 21:21:10 +0200

    added visual nodes and edges

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React from 'react';
 import './Styles/App.css';
 import Matter from "matter-js";
 
+class GraphNode {
+    constructor() {
+        return Matter.Bodies.circle(960, 540, 30,
+            {
+                restitution: 0.5,
+                label: "XD"
+            });
+    }
+}
+
+class edge {
+    constructor(nodeA, nodeB) {
+        return Matter.Constraint.create({
+            bodyA: nodeA,
+            bodyB: nodeB,
+            restitution: 0.97,
+            length: 100,
+            stiffness: 0.01,
+            render: {lineWidth: 0.5}
+        });
+    }
+}
 
 class App extends React.Component {
 
@@ -10,12 +32,16 @@ class App extends React.Component {
         this.state = {};
     }
 
+
+    //TODO:: text support https://github.com/liabru/matter-js/issues/321
+    //  ./node_modules/matter-js/src/render/render.js -> line 706
+
     componentDidMount() {
         // module aliases
         let Engine = Matter.Engine,
             Render = Matter.Render,
             World = Matter.World,
-            Bodies = Matter.Bodies,
+            // Bodies = Matter.Bodies,
             Mouse = Matter.Mouse,
             MouseConstraint = Matter.MouseConstraint;
 
@@ -35,12 +61,12 @@ class App extends React.Component {
         });
 
         // create two boxes and a ground
-        const boxA = Bodies.rectangle(400, 200, 80, 80);
-        const boxB = Bodies.rectangle(450, 50, 80, 80);
-        const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
-
-        // add all of the bodies to the world
-        World.add(engine.world, [boxA, boxB, ground]);
+        // const boxA = Bodies.rectangle(400, 200, 80, 80);
+        // const boxB = Bodies.rectangle(450, 50, 80, 80);
+        // const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+        //
+        // // add all of the bodies to the world
+        // World.add(engine.world, [boxA, boxB, ground]);
 
         // adding mouse input (constraint)
         const mouse = Mouse.create(render.canvas),
@@ -56,19 +82,24 @@ class App extends React.Component {
         World.add(engine.world, mouseConstraint);
 
         //creating spring constraint
-        const constraint = Matter.Constraint.create({
-            bodyA: boxA,
-            bodyB: boxB,
-            restitution: 0.97,
-            length: 150,
-            stiffness: 0.01
-        });
-        World.add(engine.world, constraint);
+        // const constraint = Matter.Constraint.create({
+        //     bodyA: boxA,
+        //     bodyB: boxB,
+        //     restitution: 0.97,
+        //     length: 150,
+        //     stiffness: 0.01
+        // });
 
-        var GraphNode = require("./GraphNode");
 
-        World.add(engine.world, GraphNode);
-
+        //var GraphNode = require("./GraphNode");
+        //const p = ;
+        let first = new GraphNode();
+        World.add(engine.world, first);
+        for (let i = 0; i < 10; i++) {
+            let rest = new GraphNode();
+            World.add(engine.world, rest);
+            World.add(engine.world, new edge(first, rest)); // adding edge
+        }
         Engine.run(engine);
         Render.run(render);
     }
